b1f90e3eccc46743dc87afab2cd0ef9f9e86b0c9
Alessandro Romito <alessandro.romito@mail.polimi.it>
2022-04-19 15:00:00 +0200

    Controller package intial phase

---
+++ src/main/java/it/polimi/ingsw/controller/ClientController.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.controller;
+
+import it.polimi.ingsw.observer.Observer;
+import it.polimi.ingsw.observer.ViewObserver;
+
+public class ClientController implements ViewObserver, Observer {
+}
