e408237126e412057ce55e240b3c72ee7670a585
GioeleCrespi <gioele.crespi@mail.polimi.it>
2022-04-22 15:30:53 +0200

    Card_215 implementation

--- src/main/java/it/polimi/ingsw/server/model/component/CharacterCards/Card_215.java
+++ src/main/java/it/polimi/ingsw/server/model/component/CharacterCards/Card_215.java
@@ -6,10 +6,19 @@ import it.polimi.ingsw.server.model.component.CharacterCard;
 public class Card_215 extends CharacterCard {
     private MapPositions[] old;
 
-    public Card_215(){
-        old = new MapPositions[5];
-        for(int i=2;i<=6;i++)
-
+    public Card_215(MapPositions[] old) {
+        this.old = old;
     }
 
+    public void updateOldPos (MapPositions[] old) {
+        this.old = old;
+    }
+
+    public void updateOnePos (MapPositions old, int index){
+        this.old[index] = old;
+    }
+
+    public MapPositions[] getOldPos() {
+        return old;
+    }
 }
