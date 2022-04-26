bde0f3fbbdc36d6f3a370ae86a9ddcc4b5382388
gioele.crespi@mail.polimi.it <Fcrespi1$>
2022-04-26 16:42:22 +0200

    Card_214 & 215 fix

--- src/main/java/it/polimi/ingsw/server/model/component/charactercards/Card_221.java
+++ src/main/java/it/polimi/ingsw/server/model/component/charactercards/Card_221.java
@@ -1,7 +1,5 @@
 package it.polimi.ingsw.server.model.component.charactercards;
 
-import it.polimi.ingsw.server.model.component.CharacterCard;
-
 public class Card_221 extends CharacterCard {
 
     public Card_221(int id) {
