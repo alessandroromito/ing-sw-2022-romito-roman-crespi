8af5d6d654fc9b54ef2374e71241c5e1885c0bd2
Alessandro Romito <alessandro.romito@mail.polimi.it>
2022-04-30 18:12:33 +0200

    network implementation server side

--- src/main/java/it/polimi/ingsw/network/server/Server.java
+++ src/main/java/it/polimi/ingsw/network/server/Server.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.network.server;
 
 import it.polimi.ingsw.controller.GameController;
+import it.polimi.ingsw.network.message.Message;
 
 import java.util.Map;
 
@@ -8,5 +9,15 @@ public class Server {
     private final GameController gameController;
     private final Map<String, ClientHandler> clientHandlerMap;
 
+    public void addClient(String nickname, ClientHandler clientHandler) {}
+
+    public void removeClient(String nickname) { // mandare sempre messaggio disconnessione agli altri giocatori
+        }
+
+    public void onMessageReceived(Message message){ gameController.onMessageReceived(message);}
+
+    public void onDisconnect(ClientHandler clientHandler) {}
+
+    public String getNicknameFromClientHandler(ClientHandler clientHandler){return "";}
 
 }
--- src/main/java/it/polimi/ingsw/network/server/SocketServer.java
+++ src/main/java/it/polimi/ingsw/network/server/SocketServer.java
@@ -29,7 +29,7 @@ public class SocketServer implements Runnable{
             try {
                 Socket client = serverSocket.accept();
                 client.setSoTimeout(8000);
-                SocketClientHandler clientHandler = new SocketClientHandler(this, client);
+                ClientHandler clientHandler = new ClientHandler(this, client);
                 Thread thread = new Thread (clientHandler, "ss_handler" + client.getInetAddress());
                 thread.start();
             } catch(IOException e) {
