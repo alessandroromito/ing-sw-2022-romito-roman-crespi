2dbd7821e5655aa172817525e0ed33669c88a15c
Alessandro Romito <alessandro.romito@mail.polimi.it>
2022-05-25 17:29:06 +0200

    pom.xml with javaFX adds

--- src/main/java/it/polimi/ingsw/view/gui/GUI.java
+++ src/main/java/it/polimi/ingsw/view/gui/GUI.java
@@ -3,6 +3,6 @@ package it.polimi.ingsw.view.gui;
 import it.polimi.ingsw.observer.ViewObservable;
 import it.polimi.ingsw.view.View;
 
-public class GUI implements View {
+public class GUI extends {
 
 }
