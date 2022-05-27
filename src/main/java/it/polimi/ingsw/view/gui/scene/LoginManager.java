2913ad76eee8bcc717d04587d8cecdfcc1813ea9
gioele.crespi@mail.polimi.it <Fcrespi1$>
2022-05-27 17:24:42 +0200

    LoginManager first

--- src/main/java/it/polimi/ingsw/view/gui/scene/LoginManager.java
+++ src/main/java/it/polimi/ingsw/view/gui/scene/LoginManager.java
@@ -1,4 +1,24 @@
 package it.polimi.ingsw.view.gui.scene;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class LoginManager {
+
+    @FXML
+    private Button ConnectButton;
+
+    @FXML
+    private TextField NicknameField;
+
+    @FXML
+    private TextField PortField;
+
+    @FXML
+    void tryConnection(ActionEvent event) {
+
+    }
+
 }
