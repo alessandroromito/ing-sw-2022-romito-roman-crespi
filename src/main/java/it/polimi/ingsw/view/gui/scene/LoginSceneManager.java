6552669802de5a2f8cf74f1fbb5d3b23ed3eaa3d
Alessandro Romito <alessandro.romito@mail.polimi.it>
2022-05-27 17:36:25 +0200

    LoginSceneManager name change

--- src/main/java/it/polimi/ingsw/view/gui/scene/LoginSceneManager.java
+++ src/main/java/it/polimi/ingsw/view/gui/scene/LoginSceneManager.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
-public class LoginManager {
+public class LoginSceneManager {
 
     @FXML
     private Button ConnectButton;
