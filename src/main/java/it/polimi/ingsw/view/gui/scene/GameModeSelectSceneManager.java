053bc819f906aebe46ea726cdaa86840af2eaef9
Alessandro Romito <alessandro.romito@mail.polimi.it>
2022-05-27 17:54:40 +0200

    GameModeSelectSceneManager creation

---
+++ src/main/java/it/polimi/ingsw/view/gui/scene/GameModeSelectSceneManager.java
@@ -0,0 +1,6 @@
+package it.polimi.ingsw.view.gui.scene;
+
+import it.polimi.ingsw.observer.ViewObservable;
+
+public class GameModeSelectSceneManager extends ViewObservable implements SceneManagerInterface{
+}
