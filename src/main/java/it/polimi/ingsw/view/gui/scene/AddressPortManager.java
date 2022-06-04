b89ec3d6331816710434f2e2fe5917370cb6b680
gioele.crespi@mail.polimi.it <Fcrespi1$>
2022-06-04 15:33:53 +0200

    AddressPortScene

---
+++ src/main/java/it/polimi/ingsw/view/gui/scene/AddressPortManager.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.view.gui.scene;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AddressPortManager {
+
+    @FXML
+    private Button ConnectButton;
+
+    @FXML
+    private TextField addressField;
+
+    @FXML
+    private TextField portfield;
+
+    @FXML
+    void onButtonClick(ActionEvent event) {
+
+    }
+
+}
