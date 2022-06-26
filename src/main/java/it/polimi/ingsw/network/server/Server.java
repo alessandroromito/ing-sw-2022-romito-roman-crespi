15cbf541971bee2d3f441ff3c5bf59ea6555828e
Alessandro Romito <alessandro.romito@mail.polimi.it>
2022-06-26 16:14:14 +0200

    Server checking + javadocs

--- src/main/java/it/polimi/ingsw/network/server/Server.java
+++ src/main/java/it/polimi/ingsw/network/server/Server.java
@@ -10,22 +10,33 @@ import it.polimi.ingsw.view.VirtualView;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
+/**
+ * Main server class.
+ * Able to start a socket server.
+ * It handles number of connection > 1 during game phase.
+ */
 public class Server {
     private final GameController gameController;
     private final Map<String, ClientHandler> clientHandlerMap;
-
     private final Object lock;
+
+    public static final Logger LOGGER = Logger.getLogger(Server.class.getName());
 
     public Server(GameController gameController) {
         this.gameController = gameController;
-        this.clientHandlerMap = new HashMap<>();
+        this.clientHandlerMap = new HashMap<>(); //or synchronized map?
         this.lock = new Object();
     }
 
+    /**
+     * Adds a client to be managed from server instance.
+     * @param nickname nickname associated to the client.
+     * @param clientHandler client handler associated to the client.
+     */
     public void addClient(String nickname, ClientHandler clientHandler) {
         VirtualView virtualView = new VirtualView(clientHandler);
-
         try {
             if(gameController.checkLoginNickname(nickname)){
                 gameController.addPlayer(nickname, virtualView);
@@ -40,11 +51,20 @@ public class Server {
         }
     }
 
+    /**
+     * Remove a client associated to a certain nickname.
+     * @param nickname nickname of the client to remove.
+     */
     public void removeClient(String nickname) {
         clientHandlerMap.remove(nickname);
         gameController.getVirtualViewMap().remove(nickname);
+        System.out.println(nickname + "rimosso dalla lista dei client.");
     }
 
+    /**
+     * Synchronized handler of a client disconnection.
+     * @param clientHandler clientHandler associated to the client of the disconnection request.
+     */
     public void onDisconnect(ClientHandler clientHandler) {
         synchronized (lock) {
             String nickname = getNicknameFromClientHandler(clientHandler);
@@ -63,14 +83,24 @@ public class Server {
         }
     }
 
+    /**
+     * @return the game state from GameController.
+     */
     public GameState getGameState(){
         return gameController.getGameState();
     }
 
+    /**
+     * @param clientHandler clientHandler of the client got from the clientHandlerMap.
+     * @return nickname of the client associated to clientHandler.
+     */
     public String getNicknameFromClientHandler(ClientHandler clientHandler){
         return clientHandler.getNickname();
     }
 
+    /**
+     * @return gameController
+     */
     public GameController getGameController() {
         return gameController;
     }
