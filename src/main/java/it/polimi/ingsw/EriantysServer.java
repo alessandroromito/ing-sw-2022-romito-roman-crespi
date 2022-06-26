e7a648fa3d0bf96e5fdd0c7e27665a69c470ebad
Alessandro Romito <alessandro.romito@mail.polimi.it>
2022-06-26 15:19:04 +0200

    EriantysServer checking + javadocs

--- src/main/java/it/polimi/ingsw/EriantysServer.java
+++ src/main/java/it/polimi/ingsw/EriantysServer.java
@@ -4,17 +4,24 @@ import it.polimi.ingsw.controller.GameController;
 import it.polimi.ingsw.network.server.Server;
 import it.polimi.ingsw.network.server.SocketServer;
 
+/**
+ *  Server application of the Game.
+ */
 public class EriantysServer {
 
+    /**
+     * main of the server
+     * @param args parameters assigned in the command line during execution command.
+     */
     public static void main(String[] args) {
-        int serverPort = 1511;
+        int serverPort = 1511; //default value
 
         for (int i = 0; i < args.length; i++) {
             if (args.length >= 2 && args[i].equals("--port")) {
                 try {
                     serverPort = Integer.parseInt(args[i + 1]);
                 } catch (NumberFormatException e) {
-                    System.out.println("Invalid server port specified");
+                    System.out.println("Invalid server port specified. Continuing using the default port: " + serverPort);
                 }
             }
         }
