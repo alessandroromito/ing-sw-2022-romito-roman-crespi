766531896eb5012fe067e39adbc56297fd1cfb9d
Alessandro Romito <alessandro.romito@mail.polimi.it>
2022-06-30 17:28:24 +0200

    javadocs

--- src/main/java/it/polimi/ingsw/observer/Observable.java
+++ src/main/java/it/polimi/ingsw/observer/Observable.java
@@ -5,17 +5,32 @@ import it.polimi.ingsw.network.message.Message;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class that can be observed from another class implementing the Observer interface.
+ */
 public class Observable {
     private final List<Observer> observers = new ArrayList<>();
 
+    /**
+     * Adds an observer.
+     * @param obs observer to be added to the list.
+     */
     public void addObserver(Observer obs) {
         observers.add(obs);
     }
 
+    /**
+     * Removes an observer.
+     * @param obs observer to be removed from the list.
+     */
     public void removeObserver(Observer obs) {
         observers.remove(obs);
     }
 
+    /**
+     * Notifies all the observers of the list.
+     * @param message message sent to the observers of the observer list.
+     */
     protected void notifyObserver(Message message) {
         for (Observer observer : observers) {
             observer.update(message);
--- src/main/java/it/polimi/ingsw/observer/Observer.java
+++ src/main/java/it/polimi/ingsw/observer/Observer.java
@@ -2,6 +2,9 @@ package it.polimi.ingsw.observer;
 
 import it.polimi.ingsw.network.message.Message;
 
+/**
+ * Interface implemented from class "listeners" to update the observer list of the Observable class.
+ */
 public interface Observer {
     void update(Message message);
 }
