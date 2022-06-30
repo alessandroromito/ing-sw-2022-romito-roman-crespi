2ca657e04a3078f17e81d470e42bc43e455bd3c1
Alessandro Romito <alessandro.romito@mail.polimi.it>
2022-06-30 23:39:44 +0200

    javadocs

--- src/main/java/it/polimi/ingsw/view/gui/scene/VictorySceneManager.java
+++ src/main/java/it/polimi/ingsw/view/gui/scene/VictorySceneManager.java
@@ -8,6 +8,9 @@ import javafx.stage.Stage;
 
 import java.util.Locale;
 
+/**
+ * Manager for the victory scene. Shows the winner.
+ */
 public class VictorySceneManager {
 
     private Stage stage;
@@ -18,14 +21,25 @@ public class VictorySceneManager {
     @FXML
     private Text winner;
 
+    /**
+     * Set the nickname of the winner.
+     * @param s nickname of the winner.
+     */
     public void setWinner(String s){
         winner.setText(s.toUpperCase());
     }
 
+    /**
+     * Show and wait the scene.
+     */
     public void showVictoryScene() {
         stage.showAndWait();
     }
 
+    /**
+     * Set the victory scene.
+     * @param scene to be set.
+     */
     public void setScene(Scene scene){
         stage.setScene(scene);
     }
