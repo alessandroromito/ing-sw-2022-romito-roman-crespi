5b172f4f5e83964935bcdcd155c3ed1fafd4b050
Alessandro Romito <alessandro.romito@mail.polimi.it>
2022-06-30 11:56:04 +0200

    javadocs Extra package

--- src/main/java/it/polimi/ingsw/server/extra/ComparatorAssistantCard.java
+++ src/main/java/it/polimi/ingsw/server/extra/ComparatorAssistantCard.java
@@ -4,7 +4,17 @@ import it.polimi.ingsw.server.model.player.Player;
 
 import java.util.Comparator;
 
+/**
+ * Comparator for assistant cards.
+ */
 public class ComparatorAssistantCard implements Comparator<Player> {
+
+    /**
+     * Object comparator.
+     * @param player1 the first object to be compared.
+     * @param player2 the second object to be compared.
+     * @return {code @true} if are the same, {code @false} otherwise
+     */
     public int compare(Player player1, Player player2){
         return Integer.compare(player1.getCurrentCard().getValue(), player2.getCurrentCard().getValue());
     }
